1bbe96a5580a22bfe45646275996026a7bab8ad6
Cyril Joseph <ccyyrriill83@gmail.com>
2022-03-17 19:06:34 +0530

    Add project files.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            if (num1 == 0) return BadRequest("Num1 is required");
+            if (num2 == 0) return BadRequest("Num2 is required");
+
+            return Ok(num1 + num2);
+        }
+
+        [HttpGet("sub")]
+        public IActionResult Sub(int num1, int num2)
+        {
+            if (num1 == 0) return BadRequest("Num1 is required and cannot be zero");
+            if (num2 == 0) return BadRequest("Num2 is required and cannot be zero");
+
+            return Ok(num1 - num2);
+        }
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply(int num1, int num2)
+        {
+            if (num1 == 0) return BadRequest("Num1 is required and cannot be zero");
+            if (num2 == 0) return BadRequest("Num2 is required and cannot be zero");
+
+            return Ok(num1 * num2);
+        }
+
+        [HttpGet("divide")]
+        public IActionResult Divide(int num1, int num2)
+        {
+            if (num1 == 0) return BadRequest("Num1 is required and cannot be zero");
+            if (num2 == 0) return BadRequest("Num2 is required and cannot be zero");
+
+            return Ok(num1 / num2);
+        }
+    }
+}
