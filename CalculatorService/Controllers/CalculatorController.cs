1ea5d0c80fc3197c5779eaf83d5007574e4a36fd
prabhacognizant <Prabhakaran.A@cognizant.com>
2026-01-29 17:48:27 +0530

    Update CalculatorController.cs

--- CalculatorService/Controllers/CalculatorController.cs
+++ CalculatorService/Controllers/CalculatorController.cs
@@ -14,7 +14,6 @@ namespace CalculatorService.Controllers
             if (num2 == 0) return BadRequest("Num2 is required");
             try
             {
-            
                 return Ok(num1 + num2);
             }
             catch
